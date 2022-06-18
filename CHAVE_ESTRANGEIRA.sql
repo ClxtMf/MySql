3971425a7d630369b311c3b41d631bad04c9e42d
CalixtoTech <103325008+Calixto0128@users.noreply.github.com>
2022-06-18 11:20:19 -0300

    Update CHAVE_ESTRANGEIRA.sql

--- CHAVE_ESTRANGEIRA.sql
+++ CHAVE_ESTRANGEIRA.sql
@@ -1,15 +1,63 @@
-CREATE DATABASE Editora;
+CREATE DATABASE auditora;
 
-USE editora;
+USE auditora;
 
-CREATE TABLE autores(
-id_autor INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(25) NOT NULL
+CREATE TABLE autor (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(25)
 );
 
-CREATE TABLE livros(
-id_livros INT PRIMARY KEY AUTO_INCREMENT,
-titulo VARCHAR(35) NOT NULL,
-fk_autor INT,
-FOREIGN KEY (fk_autor) REFERENCES autor (id_autor)
+
+CREATE TABLE livro(
+	id_livro INT PRIMARY KEY AUTO_INCREMENT,
+    nome_livro VARCHAR(30),
+    fk_autor INT,
+    FOREIGN KEY (fk_autor) REFERENCES autor(id)
 );
+
+
+ALTER TABLE livro AUTO_INCREMENT = 50;
+
+
+INSERT INTO autor (nome) VALUES ('Paola Joséfina'),
+('Reinaldo'), ('Felipe Agnaldo'), ('Antonia'), ('João');
+
+INSERT INTO livro (nome_livro, fk_autor) VALUES ('Algoritmos', 5),
+('BD', 1), ('Python', 3), ('C#', NULL), ('Java', 2);
+
+SELECT * FROM livro;
+
+SELECT * FROM autor;
+
+-- uma cláusula que permite a junção entre duas ou mais tabelas
+SELECT nome_livro, nome FROM livro
+INNER JOIN autor
+ON livro.fk_autor = autor.id;
+
+-- Trás todos os dados da esquerda
+SELECT nome_livro, nome FROM livro
+LEFT JOIN autor
+ON livro.fk_autor = autor.id;
+
+-- Trás todos os dados da direita
+SELECT nome_livro, nome FROM livro
+RIGHT JOIN autor
+ON livro.fk_autor = autor.id;
+
+-- Sem suporte
+SELECT nome_livro, nome FROM livro
+FULL JOIN autor
+ON livro.fk_autor = autor.id;
+
+
+
+-- Gambiarra do full join
+SELECT nome_livro, nome FROM livro
+LEFT JOIN autor
+ON livro.fk_autor = autor.id
+
+UNION
+
+SELECT nome_livro, nome FROM livro
+RIGHT JOIN autor
+ON livro.fk_autor = autor.id;
